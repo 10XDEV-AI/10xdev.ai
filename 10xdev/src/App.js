e48351d7e0221d698465c143931d02fae3ae9c0b
PrathamSutone <prathamsutone@gmail.com>
2023-03-15 18:01:01 +0530

    Base commit

--- 10xdev/src/App.js
+++ 10xdev/src/App.js
@@ -1,12 +1,18 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Chat from './components/Chat';
+import Train from './components/Train';
 
 function App() {
   return (
-    <div className="Appcontainer">
-        <Chat/>
-    </div>
-    );
+    <Router>
+      <div className="Appcontainer">
+        <Routes>
+          <Route exact path="/" element={<Chat />} />
+          <Route exact path="/train" element={<Train />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- 10xdev/src/components/Navbar.js
+++ 10xdev/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
@@ -7,7 +8,10 @@ function Navbar() {
       <div className="logo">
         10XDEV.AI
       </div>
-      <div className="searchbarcol">
+      <div className="linkscontainer">
+      <div className="linkColumn">
+        <Link className='NavLinks' to="/train">Train</Link>
+      </div>
       </div>
     </nav>
   );
