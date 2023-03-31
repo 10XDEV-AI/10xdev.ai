e0250974dc01c44fcba3bcfec441bb9bf32193e5
Sushant Kogurwar <Sushantkogurwar@gmail.com>
2023-03-31 11:21:30 +0530

    added code for retry and edit and submit

--- 10xdev/src/components/Chat.js
+++ 10xdev/src/components/Chat.js
@@ -1,69 +1,100 @@
-import ResponseContainer from './ResponseContainer/ResponseContainer';
-import UserPrompt from './UserPrompt/UserPrompt';
-import './Chat.css';
-import SearchBar from './SearchBar/SearchBar';
-import Navbar from './Navbar';
-import { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-
+import ResponseContainer from "./ResponseContainer/ResponseContainer";
+import UserPrompt from "./UserPrompt/UserPrompt";
+import "./Chat.css";
+import SearchBar from "./SearchBar/SearchBar";
+import Navbar from "./Navbar";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 function Chat() {
   const location = useLocation();
   const searchInput = location.state.searchInput;
+  // eslint-disable-next-line no-unused-vars
   const [searchResults, setSearchResults] = useState({});
   const [chatMessages, setChatMessages] = useState([]);
 
   useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const response = await fetch(`http://127.0.0.1:5000/api/data?prompt=${searchInput}`);
-      const data = await response.json();
-      console.log(data);
-      setChatMessages([{
-        index: 0,
-        prompt: <UserPrompt searchInput={searchInput} onRetry={(input) => handleRetry(input, 0)} />,
-        response: <ResponseContainer searchResults={data} />
-        }])
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  fetchData();
-}, [searchInput]);
+    const fetchData = async () => {
+      try {
+        //check for searchinput to uniquely identify the first search
+        const response = await fetch(
+          `http://127.0.0.1:5000/api/data?prompt=${searchInput}`
+        );
+        const data = await response.json();
+        console.log(data);
+        setChatMessages([
+          {
+            index: 0,
+            prompt: (
+              <UserPrompt
+                searchInput={searchInput}
+                onChildData={handleChildData}
+                onRetry={(input) => {
+                  setChatMessages((prevState) => prevState.slice(0, -1));
+                  handleSearch(input, 0);
+                }}
+              />
+            ),
+            response: <ResponseContainer searchResults={data} />,
+          },
+        ]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchInput]);
 
   const handleSearch = (input, index) => {
-      const url = `http://127.0.0.1:5000/api/data?prompt=${input}`;
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          setSearchResults(prevState => ({...prevState, [index]: data}))
-          // add the search results to chatMessages
-          setChatMessages(prevState => [...prevState, {
-            prompt: <UserPrompt searchInput={input} onRetry={(input) => handleSearch(input, index)} />,
-            response: <ResponseContainer searchResults={data} />
-          }])
-        })
-    .catch(error => console.log(error));
-}
+    const url = `http://127.0.0.1:5000/api/data?prompt=${input}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setSearchResults((prevState) => ({ ...prevState, [index]: data }));
+        // add the search results to chatMessages
+        setChatMessages((prevState) => [
+          ...prevState,
+          {
+            prompt: (
+              <UserPrompt
+                searchInput={input}
+                onChildData={handleChildData}
+                onRetry={(input) => {
+                  setChatMessages((prevState) => prevState.slice(0, -1));
+                  handleSearch(input, index);
+                }}
+              />
+            ),
+            response: <ResponseContainer searchResults={data} />,
+          },
+        ]);
+      })
+      .catch((error) => console.log(error));
+  };
+  const handleChildData = (data, index, input) => {
+    setSearchResults((prevState) => ({ ...prevState, [index]: data }));
+    
+    setChatMessages((prevState) => {
+      console.log(prevState);
+      const updatedMessages = [...prevState]; // create a copy of prevState
+      updatedMessages[index] = {
+        prompt: (
+          <UserPrompt
+            searchInput={input}
+            onChildData={handleChildData}
+            onRetry={(input) => {
+              setChatMessages((prevState) => prevState.slice(0, -1));
+              handleSearch(input, index);
+            }}
+          />
+        ),
+        response: <ResponseContainer searchResults={data} />,
+      };
+      return updatedMessages; // return the updated copy as the new state
+    });
+  };
 
-
-  const handleRetry= (input, index) => {
-      const url = `http://127.0.0.1:5000/api/data?city=${input}`;
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          setChatMessages(prevState => {
-                  const updatedMessages = [...prevState]; // create a copy of prevState
-                  updatedMessages[index] = {
-                    prompt: <UserPrompt searchInput={input} onRetry={(input) => handleSearch(input, index)} />,
-                    response: <ResponseContainer searchResults={data} />
-                  }
-                  return updatedMessages; // return the updated copy as the new state
-                })
-          console.log("retry");
-        })
-    .catch(error => console.log(error));
-}
   return (
     <div className="container">
       <div className="container">
@@ -77,10 +108,9 @@ function Chat() {
           ))}
         </div>
         <div className="spacer">
-            {/* This is a spacer div that adds empty space at the bottom */}
+          {/* This is a spacer div that adds empty space at the bottom */}
         </div>
-        <div className="footer">
-        </div>
+        <div className="footer"></div>
         <div className="searchbarrow">
           <SearchBar onSearch={handleSearch} />
         </div>
