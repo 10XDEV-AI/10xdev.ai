c78c80da738a01b9834ec60d2ebe879a6c74193e
PrathamSutone <prathamsutone@gmail.com>
2023-05-01 12:05:27 +0530

    Minor

--- 10xdev/src/Loader/Loader.js
+++ 10xdev/src/Loader/Loader.js
@@ -3,12 +3,17 @@ import './Loader.css';
 import LogViewer from './LogViewer/LogViewer';
 import SearchContext from "../context/SearchContext";
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LoadingRing = () => {
   const {setIsLoading } = useContext(SearchContext);
 
+  const navigate = useNavigate();
+
   const handleCancel = () => {
+    console.log("Cancel button clicked");
     setIsLoading(false);
+    navigate("/"); // navigate to home page
   };
 
   return (
