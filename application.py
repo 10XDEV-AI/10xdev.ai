ac39db726320e70ebc59d6bd320206dcf41b9526
PrathamSutone <prathamsutone@gmail.com>
2023-05-12 14:12:40 +0530

    AWS Amplify try 1

--- application.py
+++ application.py
@@ -10,21 +10,21 @@ from utilities.rates import set_rates, get_rates
 from syncAI import syncAI
 import os, subprocess, shutil, json
 
-app = Flask(__name__, static_folder='10xdev/build/static', template_folder='10xdev/build')
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+application = Flask(__name__, static_folder='10xdev/build/static', template_folder='10xdev/build')
+CORS(application, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/api/projectInfo', methods=['GET'])
+@application.route('/api/projectInfo', methods=['GET'])
 def get_projectInfo():
     return jsonify(getprojectInfo())
 
 
-@app.route('/api/train', methods=['GET'])
+@application.route('/api/train', methods=['GET'])
 def get_trainAI():
     path = request.args.get('path')
     print("Training AI")
@@ -32,7 +32,7 @@ def get_trainAI():
     return jsonify(a)
 
 
-@app.route('/api/Repos', methods=['GET'])
+@application.route('/api/Repos', methods=['GET'])
 def get_Repos():
     with open(os.path.join('AIFiles', 'info.json'), 'r') as f:
         info = json.load(f)
@@ -65,7 +65,7 @@ def get_Repos():
     return jsonify(directories)
 
 
-@app.route('/api/SelectRepo', methods=['GET'])
+@application.route('/api/SelectRepo', methods=['GET'])
 def select_Repos():
     Full_Path = request.args.get('Full_Path')
     try:
@@ -79,7 +79,7 @@ def select_Repos():
         return f'Error: {e}'
 
 
-@app.route('/api/Repos/<Full_path>', methods=['DELETE'])
+@application.route('/api/Repos/<Full_path>', methods=['DELETE'])
 def delete_repo(Full_path):
     if Full_path is None or Full_path.strip() == "":
         return jsonify({"message": "Invalid directory name."}), 400
@@ -111,7 +111,7 @@ def delete_repo(Full_path):
         return jsonify({"message": f"{repo_name} does not exist."}), 404
 
 
-@app.route('/api/sync', methods=['GET'])
+@application.route('/api/sync', methods=['GET'])
 def get_syncAI():
     sync_new_flag = request.args.get('sync_new')
     if sync_new_flag == 'true':
@@ -122,7 +122,7 @@ def get_syncAI():
     return jsonify({"message": message, "files": files})
 
 
-@app.route('/api/data', methods=['GET'])
+@application.route('/api/data', methods=['GET'])
 def get_data():
     prompt = request.args.get('prompt')
     print("Asking AI")
@@ -130,14 +130,14 @@ def get_data():
     return jsonify({"files": response["files"], "response": response["response"]})
 
 
-@app.route('/api/Ignore', methods=['GET'])
+@application.route('/api/Ignore', methods=['GET'])
 def get_AIIgnore():
     path = request.args.get('path')
     files2ignore, files2analyze = IgnoreAI(path)
     return jsonify({"files2ignore": files2ignore, "files2analyze": files2analyze})
 
 
-@app.route('/api/CheckAIIgnore', methods=['GET'])
+@application.route('/api/CheckAIIgnore', methods=['GET'])
 def get_CheckAIIgnore():
     path = request.args.get('path')
     if os.path.exists(path + "/.AIIgnore"):
@@ -146,44 +146,44 @@ def get_CheckAIIgnore():
         return jsonify({"AIIgnore": False})
 
 
-@app.route('/api/logs', methods=['GET'])
+@application.route('/api/logs', methods=['GET'])
 def get_logs():
     return jsonify(get_last_logs())
 
 
-@app.route('/api/setKey', methods=['GET'])
+@application.route('/api/setKey', methods=['GET'])
 def setkey():
     key = request.args.get('apikey')
     message, code = set_key(key)
     return jsonify({'message': message}), code
 
 
-@app.route('/api/getKey', methods=['GET'])
+@application.route('/api/getKey', methods=['GET'])
 def getkey():
     return jsonify(get_key())
 
 
-@app.route('/api/deleteKey', methods=['GET'])
+@application.route('/api/deleteKey', methods=['GET'])
 def deletekey():
     message, code = delete_key()
     return jsonify({'message': message}), code
 
 
-@app.route('/api/testKey', methods=['GET'])
+@application.route('/api/testKey', methods=['GET'])
 def testkey():
     key = request.args.get('apikey')
     message, code = test_key(key)
     return jsonify({'message': message}), code
 
-@app.route('/api/getRates', methods=['GET'])
+@application.route('/api/getRates', methods=['GET'])
 def getRates():
     return jsonify(get_rates())
 
 
-@app.route('/api/setRates', methods=['GET'])
+@application.route('/api/setRates', methods=['GET'])
 def setRates():
     message, code = set_rates(request.args.get('rates'))
     return jsonify({'message': message}), code
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
