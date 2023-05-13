2ff58874012a56ba668d8ce874bb9e9e22bf82d6
sushskvnitn <sushantkogurwar@gmail.com>
2023-05-13 14:40:51 +0530

    added run in bg and new alrt too

--- 10xdev/src/Loader/Loader.js
+++ 10xdev/src/Loader/Loader.js
@@ -1,31 +1,21 @@
-import React,{ useState,useEffect} from 'react';
+import React,{ useState,useContext} from 'react';
 import './Loader.css';
 import LogViewer from './LogViewer/LogViewer';
 import Welcome from "../Welcome"
-
+import SearchContext from "../context/SearchContext";
+import CoolAlert from "./CoolAlert/CoolAlert";
 const LoadingRing = () => {
   const [isclicked, setisclicked] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulating loading completion after 3 seconds
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 5000);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
+  const setclicked=()=>{setisclicked(!isclicked);};
+  const loading = useContext(SearchContext);
   return (
   <div>
   {isclicked ? <div>
+  {loading.isLoading ? <>
+    <CoolAlert message="Training in progress..." setclicked={setclicked} />
+  </>:null}
   <Welcome /> 
-  {isLoading && (
-            <div className="loading-popup">
-              <p>Loading in progress...</p>
-            </div>
-          )}
+  
   </div> :
   <div>
     <div className="loading-ring">
