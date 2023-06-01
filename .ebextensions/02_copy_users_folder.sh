f4007d73c6b5c149d11e867d06550b174b87ea44
PrathamSutone <52961179+PrathamSutone@users.noreply.github.com>
2023-06-01 16:07:04 +0530

    User ID and Password

--- .ebextensions/02_copy_users_folder.sh
+++ .ebextensions/02_copy_users_folder.sh
@@ -1,16 +1,9 @@
 files:
-  "/opt/elasticbeanstalk/hooks/appdeploy/post/99_copy_users_folder.sh":
+  "/opt/elasticbeanstalk/hooks/appdeploy/post/02_copy_users_folder.sh":
     mode: "000755"
     owner: root
     group: root
     content: |
-      #!/bin/bash
       if [ ! -d "/var/app/current/users" ]; then
         cp -R /var/backup/users /var/app/current/
-      fi
-
-      cd /var/app/current/
-
-      if [ -f /var/backup/users.zip ]; then
-        unzip -o /var/backup/users.zip -d users/
       fi
