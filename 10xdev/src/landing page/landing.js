d2b88408682b5a59e6f702058585c98b7d7549ff
PrathamSutone <52961179+PrathamSutone@users.noreply.github.com>
2023-06-01 22:39:52 +0530

    User ID and Password

--- 10xdev/src/landing page/landing.js
+++ 10xdev/src/landing page/landing.js
@@ -9,11 +9,11 @@ const LandingPage = () => {
       <h1 className="landing-text">Use the power of Auto GPT to become a 10X developer</h1>
       <p className="landing-text">Click on the button below to get started!</p>
         {/*a1 - DEV a2- UAT a3- Production
+        <a className="landing-link" href="https://10xdevai.auth.eu-north-1.amazoncognito.com/oauth2/authorize?client_id=5jjlg8ig3demen3praeuur0lea&response_type=token&scope=aws.cognito.signin.user.admin+email+openid&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2Fwelcome">
         <a className="landing-link" href="https://10xdevai.auth.eu-north-1.amazoncognito.com/oauth2/authorize?client_id=5jjlg8ig3demen3praeuur0lea&response_type=token&scope=aws.cognito.signin.user.admin+email+openid&redirect_uri=http%3A%2F%2Ftest.10xdevai.com%2Fwelcome">
         <a className="landing-link" href="https://10xdevai.auth.eu-north-1.amazoncognito.com/oauth2/authorize?client_id=5jjlg8ig3demen3praeuur0lea&response_type=token&scope=aws.cognito.signin.user.admin+email+openid&redirect_uri=https%3A%2F%2F10xdevai.com%2Fwelcome">
-        <a className="landing-link" href="https://10xdevai.auth.eu-north-1.amazoncognito.com/oauth2/authorize?client_id=5jjlg8ig3demen3praeuur0lea&response_type=token&scope=aws.cognito.signin.user.admin+email+openid&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2Fwelcome">
         */}
-        <a className="landing-link" href="https://10xdevai.auth.eu-north-1.amazoncognito.com/oauth2/authorize?client_id=5jjlg8ig3demen3praeuur0lea&response_type=token&scope=aws.cognito.signin.user.admin+email+openid&redirect_uri=https%3A%2F%2F10xdevai.com%2Fwelcome">
+        <a className="landing-link" href="https://10xdevai.auth.eu-north-1.amazoncognito.com/oauth2/authorize?client_id=5jjlg8ig3demen3praeuur0lea&response_type=token&scope=aws.cognito.signin.user.admin+email+openid&redirect_uri=http%3A%2F%2Ftest.10xdevai.com%2Fwelcome">
             <button className="landing-button">Get Started</button>
         </a>
     </div>
