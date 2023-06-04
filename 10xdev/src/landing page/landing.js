341a6efbc1e6b2ea303b163f8230ca24d9aa9964
PrathamSutone <52961179+PrathamSutone@users.noreply.github.com>
2023-06-04 17:04:28 +0530

    User ID and Password

--- 10xdev/src/landing page/landing.js
+++ 10xdev/src/landing page/landing.js
@@ -23,7 +23,7 @@ const LandingPage = () => {
       <a
         className="landing-link"
       
-        href={`https://10xdevgoogleauth.auth.eu-north-1.amazoncognito.com/oauth2/authorize?client_id=7rj9u2ntqm57fsqeod3lmgloag&response_type=code&scope=aws.cognito.signin.user.admin+email+openid&redirect_uri=${redirectUri}`}
+        href={`https://10xdevgoogleauth.auth.eu-north-1.amazoncognito.com/oauth2/authorize?client_id=7rj9u2ntqm57fsqeod3lmgloag&response_type=token&scope=aws.cognito.signin.user.admin+email+openid&redirect_uri=${redirectUri}`}
         >
         <button className="landing-button">Get Started</button>
       </a>
