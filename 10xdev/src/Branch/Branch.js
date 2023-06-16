07f6287f3e3493a73334cb9c07594e50814650b1
PrathamSutone <52961179+PrathamSutone@users.noreply.github.com>
2023-06-17 04:44:48 +0530

    Prod bug fixes

--- 10xdev/src/Branch/Branch.js
+++ 10xdev/src/Branch/Branch.js
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom';
 import Navbar from '../Navbar';
 import SearchContext from '../context/SearchContext';
 import { callAPI } from '../api';
-import './Branch.css';
 
 const Branch = () => {
   const { path, isLoading, setIsLoading } = useContext(SearchContext);
@@ -26,7 +25,7 @@ const Branch = () => {
     fetchData();
   }, [path, setIsLoading]);
 
-  const handleselect = async (branch) => {
+  const handleSelect = async (branch) => {
     try {
       console.log(branch);
       const data = await callAPI(`/api/setBranch?path=${path}&branch=${branch}`);
@@ -40,21 +39,26 @@ const Branch = () => {
   return (
     <div>
       <Navbar />
-      <div className="branch-container">
-        {branches.length > 0 ? (
-          <div className="ignoretips">
-            <h2>Select Branch</h2>
-            <ul className="branch-list">
-              {branches.map((branch) => (
-                <li key={branch}>
-                  <button className="branch-buttons" onClick={() => handleselect(branch)}>
-                    {branch}
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ) : null}
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="max-w-md  w-full bg-white border-dashed border-gray-300 border-2 rounded-lg p-6">
+          {branches.length > 0 ? (
+            <div>
+              <h2 className="text-2xl font-semibold text-gray-800">Select Branch</h2>
+              <ul className="mt-4">
+                {branches.map((branch) => (
+                  <li key={branch}>
+                    <button
+                      className="w-full bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 px-4 rounded mt-2 focus:outline-none focus:ring"
+                      onClick={() => handleSelect(branch)}
+                    >
+                      {branch}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   );
