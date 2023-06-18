aa3381ea5597d49db9fa72399a51d19806640f40
sushskvnitn <sushantkogurwar@gmail.com>
2023-06-19 02:31:08 +0530

    dn with login and getting access of repo and storing acceess token and getuser

--- 10xdev/src/Clone.js
+++ 10xdev/src/Clone.js
@@ -1,22 +1,28 @@
-import React, { useState, useContext } from 'react';
+
+import React, { useState, useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Clone.css';
 import Navbar from './Navbar';
 import SearchContext from './context/SearchContext';
 import LoadingRing from './Loader/Loader';
 import { callAPI } from './api';
+import { BsGithub } from 'react-icons/bs';
+import { FaGitlab } from 'react-icons/fa';
+import cookies from 'js-cookie';
 
 const Clone = () => {
   const { isLoading, setIsLoading } = useContext(SearchContext);
   const [branches, setBranches] = useState([]);
   const [input, setInput] = useState('');
   const navigate = useNavigate();
-
+  const client_id = 'eaae8a43278892ed15e1';
+  const [userdata, setUserData] = useState(null);
+  const [isauthenticated, setIsAuthenticated] = useState(false);
   const handleClone = async () => {
     setIsLoading(true);
     try {
-      const data = await callAPI(`/api/clone?path=${input}`,{
-      method: 'GET',
+      const data = await callAPI(`/api/clone?path=${input}`, {
+        method: 'GET',
       });
       console.log(data);
       setBranches(data);
@@ -26,6 +32,56 @@ const Clone = () => {
     setIsLoading(false);
   };
 
+  const loginWithGithub = () => {
+    window.location.assign(
+      `https://github.com/login/oauth/authorize?client_id=${client_id}&scope=repo`
+    );
+  };
+
+ 
+  useEffect(() => {
+    cookies.remove('accesstoken');
+    setIsAuthenticated(false);
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const code = urlParams.get('code');
+    if (code) {
+      
+      const fetchToken = async () => {
+        const response = await callAPI(`/api/github?code=${code}`, {
+          method: 'GET',
+        });
+        if(response.access_token){
+          cookies.set('accesstoken', response.access_token.toString());
+        }
+      };
+      fetchToken();
+      setTimeout(() => {
+        getGithubUser();
+        setIsAuthenticated(true); 
+    }, 3000);
+    }
+   
+    
+    
+  }, []);
+
+  const getGithubUser = async() => {
+    const accessToken = cookies.get('accesstoken');
+    console.log(accessToken);
+    try {
+      const response = await callAPI(`/api/github/getuser?access_token=${accessToken}`, {
+        method: 'GET',
+      });
+      console.log(response);
+        setUserData(response);
+      return response;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
+
   const handleInputChange = (event) => {
     setInput(event.target.value);
   };
@@ -33,8 +89,8 @@ const Clone = () => {
   const handleselect = async (branch) => {
     try {
       console.log(branch);
-      const data = await callAPI(`/api/setBranch?path=${input}&branch=${branch}`,{
-      method: 'GET',
+      const data = await callAPI(`/api/setBranch?path=${input}&branch=${branch}`, {
+        method: 'GET',
       });
       console.log(data);
       navigate(`/repos`);
@@ -42,7 +98,6 @@ const Clone = () => {
       console.error(error);
     }
   };
-
 
   return (
     <div>
@@ -52,8 +107,8 @@ const Clone = () => {
       ) : (
         <div>
           <div className="GetIgnorecontainer">
+            <div className="pathsearchlabel">Add public repo URL:</div>
             <label className="pathsearchrow">
-              <div className="pathsearchlabel">Clone Repo :</div>
               <input
                 type="text"
                 value={input}
@@ -62,37 +117,95 @@ const Clone = () => {
               />
             </label>
             <button onClick={handleClone} className="gitIgnorebutton">
-              Clone Repository
+              clone
             </button>
           </div>
-          <div className="branch-container">
-              {branches.length > 0 ? (
-                <div className="ignoretips">
-                    <h2> Select your desired branch </h2>
-                  {(
-                    <ul className="branch-list">
-                      {branches.map((branch,) => (
-                        <li key = {branch}>
-                          <button className="branch-buttons" onClick={() => handleselect(branch)}>{branch}</button>
-                        </li>
-                      ))}
-                    </ul>
-                  )}
+          <br />
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'column',
+              marginTop: '20px',
+            }}
+          >
+            <div>
+              <h3>Add Your Private Repository</h3>
+            </div>
+            {isauthenticated ? (
+              <div></div>
+              ) : (
+                <div>
+                <button
+              onClick={loginWithGithub}
+              className="gitIgnorebutton"
+              style={{
+                marginBottom: '20px',
+              }}
+            >
+              Connect with Github <BsGithub />
+            </button>
+            <button className="gitIgnorebutton">
+              Connect with Gitlab <FaGitlab />
+            </button>
                 </div>
-              ) :
-              (
-              <div className="ignoretips">
-              <h2> 💡Tips on training Repository</h2>
-                  <ul>
-                      <li> Add big folders and packages not in use to .AIIgnore file </li>
-                      <li> Add any other files that are not required for training </li>
-                      <li> Break down big files to smaller files </li>
-                      <li> Works well with small files </li>
-                      <li> Works well for files with comments </li>
-                  </ul>
+                )}
+            
+          </div>
+          {userdata && (
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+                margin: '20px',
+                backgroundColor: '#f5f5f5',
+              }}
+            >
+              <div
+              >
+                <div>
+                </div>
+                <img src={userdata.avatar_url} height="50" style={{
+                  borderRadius: '50%',
+                }} width="50" alt="avatar" />
+                <div>{userdata.name}</div>
+                <div>{userdata.bio}</div>
+                <div>Public Repos :{userdata.public_repos}</div>
               </div>
-              )
-              }
+            </div>
+          )}
+          <div className="branch-container">
+            {branches.length > 0 ? (
+              <div className="ignoretips">
+                <h2> Select your desired branch </h2>
+                {branches.map((branch) => (
+                  <ul key={branch} className="branch-list">
+                    <li>
+                      <button
+                        className="branch-buttons"
+                        onClick={() => handleselect(branch)}
+                      >
+                        {branch}
+                      </button>
+                    </li>
+                  </ul>
+                ))}
+              </div>
+            ) : (
+              <div className="ignoretips">
+                <h2> 💡Tips on training Repository</h2>
+                <ul>
+                  <li> Add big folders and packages not in use to .AIIgnore file </li>
+                  <li> Add any other files that are not required for training </li>
+                  <li> Break down big files to smaller files </li>
+                  <li> Works well with small files </li>
+                  <li> Works well for files with comments </li>
+                </ul>
+              </div>
+            )}
           </div>
         </div>
       )}
