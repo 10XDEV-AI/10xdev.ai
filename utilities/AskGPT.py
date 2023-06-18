e50dd1b93cf8e6a213f23589a15c189c51f51157
PrathamSutone <52961179+PrathamSutone@users.noreply.github.com>
2023-06-18 06:35:20 +0530

    Prod bug fixes

--- utilities/AskGPT.py
+++ utilities/AskGPT.py
@@ -3,14 +3,11 @@ import openai
 from utilities.keyutils import get_key
 from utilities.tokenCount import tokenCount
 
-
-def AskGPT(email, system_message="", prompt="Hi", temperature=0, max_tokens=256, retrys=3, delay=20):
+def AskGPT(email, system_message="", prompt="Hi", temperature=0, max_tokens=256, retrys=3, delay=20,model = "gpt-3.5-turbo"):
     if tokenCount(str(prompt) + str(system_message)) > 2500:
         if tokenCount(str(prompt) + str(system_message)) > 15000:
-            return "There was error with token limits, Please try again with another prompt"
+            return "There was an error with token limits. Please try again with another prompt."
         model = "gpt-3.5-turbo-16k"
-    else:
-        model = "gpt-3.5-turbo"
 
     openai.api_key = get_key(email)
     try:
@@ -24,8 +21,12 @@ def AskGPT(email, system_message="", prompt="Hi", temperature=0, max_tokens=256,
         return response["choices"][0]["message"]['content']
     except Exception as e:
         print(f"Encountered error: {e}")
-        if retrys >= 0:
-            print("Retrying in %f seconds...",delay)
+        if "maximum context length" in str(e):
+            time.sleep(delay)
+            print("Retrying in %f seconds wit larger model", delay)
+            return AskGPT(email, system_message, prompt, temperature, max_tokens, retrys - 1, delay, model = "gpt-3.5-turbo-16k")
+        elif retrys >= 0:
+            print("Retrying in %f seconds...", delay)
             time.sleep(delay)
             return AskGPT(email, system_message, prompt, temperature, max_tokens, retrys - 1, delay)
         else:
