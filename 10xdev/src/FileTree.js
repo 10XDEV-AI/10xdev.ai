b9cf0c8960ef0e56e18d7785d8e3d52245d81aa2
PrathamSutone <52961179+PrathamSutone@users.noreply.github.com>
2023-06-23 15:26:44 +0530

    Fixes for 401

--- 10xdev/src/FileTree.js
+++ 10xdev/src/FileTree.js
@@ -28,7 +28,28 @@ function DirectoryTreeView(props) {
           nodeRenderer={({ element, isBranch, isExpanded, getNodeProps, level }) => (
             <div {...getNodeProps()} style={{ paddingLeft: calculateIndentation(level) }}>
               <label className="flex items-center cursor-pointer" onClick={() => console.log(element.name)}>
-                <input type="checkbox" onChange={(e) => console.log(e.target.checked)}  className="m-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"/>
+                <input type="checkbox"
+                       className="m-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
+                       onChange={(e) => {
+                         const isChecked = e.target.checked;
+                         const { id, isBranch, children } = element;
+                         if (isBranch) {
+                           // If the clicked node is a folder, update all files in that folder
+                           const flattenedChildren = flattenTree(children);
+                           flattenedChildren.forEach((child) => {
+                             const { id, isBranch } = child;
+                             if (!isBranch) {
+                               const { checked } = getNodeProps({ nodeId: id });
+                               if (checked !== isChecked) {
+                                 getNodeProps({ nodeId: id, checked: isChecked });
+                               }
+                             }
+                           });
+                         } else {
+                           // If the clicked node is a file, update its checked status
+                           getNodeProps({ nodeId: id, checked: isChecked });
+                         }
+                       }}/>
                 {isBranch ? <FolderIcon isOpen={isExpanded} /> : <FileIcon filename={element.name} />}
                 {element.name}
               </label>
