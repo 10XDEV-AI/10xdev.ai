b7881d69bdf6bf41aea5848cd08833cd7d237f70
PrathamSutone <52961179+PrathamSutone@users.noreply.github.com>
2023-06-23 15:58:51 +0530

    Fixes for 401

--- 10xdev/src/Welcome.js
+++ 10xdev/src/Welcome.js
@@ -164,6 +164,7 @@ export const Welcome = () => {
             <div className="h-[60vh] overflow-y-auto overflow-x-hidden ">
               <div className="flex items-center justify-center">
               <h2 className="text-xl font-bold mb-2">Your code:</h2>
+              // Filter Icon
               <button className="bg-blue-900 text-white font-bold px-1 rounded-md ml-auto hover:bg-blue-600 shadow-md flex" >
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 p-1 my-1">
                   <path fillRule="evenodd" d="M2.628 1.601C5.028 1.206 7.49 1 10 1s4.973.206 7.372.601a.75.75 0 01.628.74v2.288a2.25 2.25 0 01-.659 1.59l-4.682 4.683a2.25 2.25 0 00-.659 1.59v3.037c0 .684-.31 1.33-.844 1.757l-1.937 1.55A.75.75 0 018 18.25v-5.757a2.25 2.25 0 00-.659-1.591L2.659 6.22A2.25 2.25 0 012 4.629V2.34a.75.75 0 01.628-.74z" clipRule="evenodd" />
