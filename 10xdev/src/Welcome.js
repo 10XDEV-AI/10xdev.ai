38dfdb852d9a5228c57dff567e57d986f80c595e
PrathamSutone <52961179+PrathamSutone@users.noreply.github.com>
2023-07-02 05:07:37 +0530

    Minor Fixes

--- 10xdev/src/Welcome.js
+++ 10xdev/src/Welcome.js
@@ -18,6 +18,7 @@ import { FaStar, FaQuestion, FaGamepad, FaBug, FaFlask, FaBook, FaMagic } from '
 export const Welcome = () => {
   const { setSearchTerm, isLoading, setIsLoading,showSync, setShowSync ,isnewuser, setIsNewUser,showRepos, setShowRepos } = useContext(SearchContext);
   const [input, setInput] = useState("");
+  const [isTreeLoading, setIsTreeLoading] = useState(true);
   const [typingStarted, setTypingStarted] = useState(false);
   const navigate = useNavigate();
 
@@ -136,9 +137,11 @@ export const Welcome = () => {
 
   const getTreeData = async () => {
     try {
+      setIsTreeLoading(true);
       const data = await callAPI(`/api/FilesToAnalyzedata?path=`);
       const tree = convertToTree(data.files2analyze);
       setTreeData(tree);
+      setIsTreeLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -206,7 +209,12 @@ export const Welcome = () => {
                       </button>
                       </div>
                       <div className="bg-white border border-gray-400 h-[50vh] overflow-y-auto rounded-lg shadow-md">
-                      <FileTree data={treeData} showCheckboxes={showCheckboxes} />
+                          {isTreeLoading ? (
+                          <div className="flex justify-center items-center h-[50vh]">
+                          Loading......
+                          </div>
+                          ) : (  <FileTree data={treeData} showCheckboxes={showCheckboxes} />
+                          )}
                       </div>
                     </div>
                     <div className="flex items-center text-blue-900 justify-center">
@@ -222,8 +230,6 @@ export const Welcome = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4 m-1">
                           <path fill-rule="evenodd" d="M10 4.5c1.215 0 2.417.055 3.604.162a.68.68 0 01.615.597c.124 1.038.208 2.088.25 3.15l-1.689-1.69a.75.75 0 00-1.06 1.061l2.999 3a.75.75 0 001.06 0l3.001-3a.75.75 0 10-1.06-1.06l-1.748 1.747a41.31 41.31 0 00-.264-3.386 2.18 2.18 0 00-1.97-1.913 41.512 41.512 0 00-7.477 0 2.18 2.18 0 00-1.969 1.913 41.16 41.16 0 00-.16 1.61.75.75 0 101.495.12c.041-.52.093-1.038.154-1.552a.68.68 0 01.615-.597A40.012 40.012 0 0110 4.5zM5.281 9.22a.75.75 0 00-1.06 0l-3.001 3a.75.75 0 101.06 1.06l1.748-1.747c.042 1.141.13 2.27.264 3.386a2.18 2.18 0 001.97 1.913 41.533 41.533 0 007.477 0 2.18 2.18 0 001.969-1.913c.064-.534.117-1.071.16-1.61a.75.75 0 10-1.495-.12c-.041.52-.093 1.037-.154 1.552a.68.68 0 01-.615.597 40.013 40.013 0 01-7.208 0 .68.68 0 01-.615-.597 39.785 39.785 0 01-.25-3.15l1.689 1.69a.75.75 0 001.06-1.061l-2.999-3z" clip-rule="evenodd" />
                         </svg>
-
-
                       </button>
                     </div>
                     </>
