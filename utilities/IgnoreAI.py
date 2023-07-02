c8f4f90e211b70c8fc164fb5daaf5f4eff3e08b1
PrathamSutone <52961179+PrathamSutone@users.noreply.github.com>
2023-07-02 08:00:20 +0530

    Minor Fixes

--- utilities/IgnoreAI.py
+++ utilities/IgnoreAI.py
@@ -1,10 +1,11 @@
 import chardet, os, fnmatch, re
 from utilities.tokenCount import tokenCount
 from concurrent.futures import ThreadPoolExecutor
+from utilities.notebook_utils import convert_ipynb_to_python
 
 def process_file(root, filename, path, user_logger):
     # Skip file types like jpg, svg, gif, etc.
-    if filename.endswith(('.jpg', '.svg', '.gif', '.png', '.jpeg', '.ico', '.pdf', '.docx', '.doc', '.xlsx', '.xls', '.pptx', '.ppt', '.txt', '.zip', '.rar', '.7z', '.mp4', '.webm', '.avi', '.mkv', '.flv', '.mpeg', '.mpg', '.ogg', '.ogv', '.webm', '.wmv', '.ttf', '.bmp', '.ipynb' )):
+    if filename.endswith(('.jpg', '.svg', '.gif', '.png', '.jpeg', '.ico', '.pdf', '.docx', '.doc', '.xlsx', '.xls', '.pptx', '.ppt', '.txt', '.zip', '.rar', '.7z', '.mp4', '.webm', '.avi', '.mkv', '.flv', '.mpeg', '.mpg', '.ogg', '.ogv', '.webm', '.wmv', '.ttf', '.bmp' )):
         return {"Path": os.path.relpath(os.path.join(root, filename), path), "Tokens": 'NA', "Sign": 'ℹ️'}
 
     if not filename.endswith(('.c', '.cpp', '.h', '.java', '.js', '.css', '.html', '.htm', '.xml', '.json', '.sql', '.md', '.yml', '.yaml', '.sh', '.bat', '.jsx', '.txt', '.php', '.rb', '.pl', '.swift', '.go', '.cs', '.vb', '.lua', '.scala', '.rust', '.ts', '.scss', '.sass', '.less', '.coffee', '.asm', '.r', '.pyc', '.class', '.dll', '.exe', '.bat', '.ps1')):
@@ -25,6 +26,14 @@ def process_file(root, filename, path, user_logger):
         file_contents = open(os.path.join(root, filename), 'r').read()
     except UnicodeDecodeError:
         return {"Path": os.path.relpath(os.path.join(root, filename), path), "Tokens": 'NA', "Sign": 'info'}
+
+    if filename.endswith("ipynb"):
+        file_contents = convert_ipynb_to_python(os.path.join(root, filename))
+        if len(re.split(r'[.,;\n\s]+', file_contents)) > 15000:
+            return {"Path": os.path.relpath(os.path.join(root, filename), path), "Tokens": 'NA', "Sign": 'warning'}
+
+        tick_or_cross = 'success' if tokenCount(file_contents) < 15000 else 'warning'
+        return {"Path": os.path.relpath(os.path.join(root, filename), path), "Tokens": tokenCount(file_contents), "Sign": tick_or_cross}
 
     if len(re.split(r'[.,;\n\s]+', file_contents)) > 15000:
         return {"Path": os.path.relpath(os.path.join(root, filename), path), "Tokens": 'NA', "Sign": 'warning'}
@@ -79,6 +88,6 @@ def parse_ignore_file(file_path):
 def is_file_ignored(filename):
     ignored_extensions = ('.jpg', '.svg', '.gif', '.png', '.jpeg', '.ico', '.pdf', '.docx', '.doc', '.xlsx', '.xls',
                           '.pptx', '.ppt', '.txt', '.zip', '.rar', '.7z', '.mp4', '.webm', '.avi', '.mkv', '.flv',
-                          '.mpeg', '.mpg', '.ogg', '.ogv', '.webm', '.wmv', 'ttf' '.ipynb')
+                          '.mpeg', '.mpg', '.ogg', '.ogv', '.webm', '.wmv', 'ttf')
     return filename.endswith(ignored_extensions)
 
