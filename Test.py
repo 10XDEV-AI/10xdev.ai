bf2eb356b870ea872fa2b1ea9f902b30ed9127e1
PrathamSutone <52961179+PrathamSutone@users.noreply.github.com>
2023-07-16 22:06:44 +0530

    Train AI Parallel Testing

---
+++ Test.py
@@ -0,0 +1,3 @@
+from utilities.folder_tree_structure import generate_folder_structure
+
+print(generate_folder_structure("prathamthepro@gmail.com","10xdev.ai"))
--- utilities/folder_tree_structure.py
+++ utilities/folder_tree_structure.py
@@ -1,25 +1,31 @@
 import os
-def generate_folder_structure(root_folder):
+from utilities.IgnoreAI import parse_ignore_file
+
+def generate_folder_structure(email, root_folder):
     log = []
 
-    def traverse_folder(folder, indent_level):
-        indent = "  " * indent_level
+    def traverse_folder(root_folder, folder, indent_level):
+        indent = " " * indent_level
+
+        # Ignore directories starting with "."
+        if os.path.basename(folder).startswith('.'):
+            return
+
         log.append(f"{indent}- {folder}")
 
         for item in os.listdir(folder):
             item_path = os.path.join(folder, item)
 
             if os.path.isdir(item_path):
-                traverse_folder(item_path, indent_level + 1)
+                relpath = os.path.relpath(item_path, root_folder)
+                if not AIignore(os.path.normpath(relpath)):
+                    traverse_folder(root_folder, item_path, indent_level + 1)
             else:
-                log.append(f"{indent}  - {item}")
+                log.append(f"{indent} - {item}")
 
-    traverse_folder(root_folder, 0)
+    ignore_file_path = os.path.join("../user", email, '.AIIgnore' + root_folder)
+    AIignore = parse_ignore_file(ignore_file_path) if os.path.exists(ignore_file_path) else lambda x: False
+    root_folder = os.path.join("../user", email, root_folder)
+    traverse_folder(root_folder, root_folder, 0)
+
     return "\n".join(log)
-
-
-
-if __name__ == "__main__":
-    log_output =  generate_folder_structure("/Users/prathameshsutone/Download/10xdev.ai")
-    print(log_output)
-
