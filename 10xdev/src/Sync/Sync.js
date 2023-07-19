cb457fa7202e1b87f214ecc467bf3bd729779ad7
PrathamSutone <52961179+PrathamSutone@users.noreply.github.com>
2023-07-20 01:10:44 +0530

    Commit Hash

--- 10xdev/src/Sync/Sync.js
+++ 10xdev/src/Sync/Sync.js
@@ -7,7 +7,7 @@ import { FaFile } from 'react-icons/fa';
 import {useNavigate} from 'react-router-dom';
 
 function Sync(handleSyncClick) {
-  const { showSync, setShowSync , setPath} = useContext(SearchContext);
+  const { showSync, setShowSync , setPath, commitHash,setCommitHash } = useContext(SearchContext);
   const [isSyncing, setIsSyncing] = useState(false);
   const [showTick, setShowTick] = useState(false);
   const [showWarning, setShowWarning] = useState(false);
@@ -62,8 +62,10 @@ function Sync(handleSyncClick) {
           setShowWarning(false);
           setShowTick(true);
           setNewFiles([]);
+          const data = await callAPI('/api/projectInfo');
+          setCommitHash(data.latest_commit_hash)
           setTimeout(() => {
-            setShowSync(false);
+          setShowSync(false);
           }, 5000); // Set showSync to false after 5 seconds
         } else if (data.message === "NEW") {
           setShowTick(false);
