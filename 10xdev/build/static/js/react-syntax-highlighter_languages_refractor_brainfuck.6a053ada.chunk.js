374a21d35eb1fd1f730d07f38ab1bb51e463b0df
PrathamSutone <52961179+PrathamSutone@users.noreply.github.com>
2023-07-24 01:32:20 +0530

    Filter Improvements

--- 10xdev/build/static/js/react-syntax-highlighter_languages_refractor_brainfuck.6a053ada.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_10xdev=self.webpackChunk_10xdev||[]).push([[5539],{2689:function(e){function a(e){e.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}e.exports=a,a.displayName="brainfuck",a.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.6a053ada.chunk.js.map
--- 10xdev/build/static/js/react-syntax-highlighter_languages_refractor_hsts.01718292.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_10xdev=self.webpackChunk_10xdev||[]).push([[3140],{7761:function(e){function a(e){e.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\d{8,}/,alias:"selector"},unsafe:{pattern:/\d{1,7}/,alias:"function"}}}e.exports=a,a.displayName="hsts",a.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts.01718292.chunk.js.map
