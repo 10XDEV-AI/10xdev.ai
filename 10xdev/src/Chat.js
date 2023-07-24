ed17f76aebf15a828d4e92f1a74a7c29e638e62c
PrathamSutone <52961179+PrathamSutone@users.noreply.github.com>
2023-07-25 02:30:19 +0530

    Bug Fixes

--- 10xdev/src/Chat.js
+++ 10xdev/src/Chat.js
@@ -38,6 +38,14 @@ export const Chat = () => {
     setShowLeftWelcome(!showLeftWelcome);
   };
 
+  const handleRemoveFile = (index) => {
+    setCheckedFiles((prevState) => {
+      const updatedFiles = [...prevState];
+      updatedFiles.splice(index, 1);
+      return updatedFiles;
+    });
+  };
+
   const handleSearch = async (input) => {
     console.log("searching for");
     console.log(input);
@@ -125,11 +133,6 @@ export const Chat = () => {
       const results = JSON.stringify(data.response);
       const files = data.files;
       const code = data.referenced_code;
-      console.log(results);
-      console.log(files);
-      console.log(code);
-      console.log("index");
-      console.log(index);
       setChatMessages((prevState) => {
         const updatedMessages = [...prevState];
         updatedMessages[index] = {
@@ -184,17 +187,36 @@ export const Chat = () => {
                     <div className={`${showLeftWelcome ? "w-6/12 absolute right-0 " : "w-full"}`}>
                       <Navbar LoadProjectInfo="True" file="chat" onHamburgerClick={handleHamburgerClick} />
                       <div class="w-full bg-slate-50 p-4 rounded-lg ">
-                        <div class="flex flex-wrap gap-2 justify-center items-center" onClick={handleHamburgerClick}>
+                        <div class="flex flex-wrap gap-2 justify-center items-center" >
                           {checkedFiles.length>0&&
-                              <div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 my-1">
+                              <div onClick={handleHamburgerClick}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 my-1">
                                      <path fillRule="evenodd" d="M2.628 1.601C5.028 1.206 7.49 1 10 1s4.973.206 7.372.601a.75.75 0 01.628.74v2.288a2.25 2.25 0 01-.659 1.59l-4.682 4.683a2.25 2.25 0 00-.659 1.59v3.037c0 .684-.31 1.33-.844 1.757l-1.937 1.55A.75.75 0 018 18.25v-5.757a2.25 2.25 0 00-.659-1.591L2.659 6.22A2.25 2.25 0 012 4.629V2.34a.75.75 0 01.628-.74z" clipRule="evenodd" />
                                    </svg></div>}
                           {checkedFiles.map((file, index) => (
-                            <div
-                              key={index}
-                              class="text-blue-500 border border-blue-500 py-1 px-3 rounded-full text-sm font-semibold"
-                            >
-                              {file}
+                            <div key={index} className="flex items-center gap-2">
+                              <div className="flex text-blue-500 border border-blue-500 py-1 pl-6 gap-1 pr-2 rounded-full text-sm font-semibold justify-center items-center">
+                                {file}
+                                  <button
+                                      className="text-slate-50 hover:text-blue-500"
+                                      onClick={() => handleRemoveFile(index)}
+                                  >
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    className="w-4 h-4"
+                                  >
+                                    <path
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      strokeWidth={2}
+                                      d="M6 18L18 6M6 6l12 12"
+                                    />
+                                  </svg>
+                                </button>
+                              </div>
+
                             </div>
                           ))}
                         </div>
