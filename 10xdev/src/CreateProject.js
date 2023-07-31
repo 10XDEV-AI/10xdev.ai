c4b025953bac1ea434c72fb42c39db6244bbf592
PrathamSutone <52961179+PrathamSutone@users.noreply.github.com>
2023-08-01 01:14:56 +0530

    New Create Project

--- 10xdev/src/CreateProject.js
+++ 10xdev/src/CreateProject.js
@@ -3,9 +3,9 @@ import { callAPI } from "./api";
 
 function CreateProject() {
   const [prompt, setPrompt] = useState("");
-  const [clarifyingQuestions, setClarifyingQuestions] = useState([]);
-  const [userAnswers, setUserAnswers] = useState([]);
-  const [userClarifyingAnswers, setUserClarifyingAnswers] = useState([]);
+  const [clarifyingQuestions, setClarifyingQuestions] = useState("");
+  const [userAnswers, setUserAnswers] = useState("");
+  const [userClarifyingAnswers, setUserClarifyingAnswers] = useState("");
 
   const handlePromptChange = (event) => {
     setPrompt(event.target.value);
@@ -24,15 +24,14 @@ function CreateProject() {
         body: body,
       });
       console.log(response);
+      setClarifyingQuestions(response);
     } catch (error) {
       console.error(error);
     }
   };
 
-  const handleAnswerChange = (event, index) => {
-    const answers = [...userAnswers];
-    answers[index] = event.target.value;
-    setUserAnswers(answers);
+  const handleAnswerChange = (event) => {
+    setUserClarifyingAnswers(event.target.value);
   };
 
   const handleClarifyingQuestionsSubmit = async () => {
@@ -52,43 +51,46 @@ function CreateProject() {
   };
 
   return (
-    <div>
-      <h1 className="w-full text-2xl">New Page</h1>
-      <h2 htmlFor="prompt" className="w-full text-xl">Enter a project prompt:</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          id="prompt"
-          value={prompt}
-          onChange={handlePromptChange}
-          className="border rounded px-4 py-2 w-full"
-        />
-        <button type="submit" className="bg-blue-900 text-white px-4 py-2 mt-4 rounded">
-          Submit
-        </button>
-      </form>
+    <div className="p-10">
+    <h1 class="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl ">Create your project in just few steps</h1>
+    <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl ">Describe your project. AI will ask clarifying questions next</p>
+        <form onSubmit={handleSubmit}>
+           <div class="w-full mb-4  border border-gray-200 rounded-lg bg-gray-50 ">
+               <div class="px-4 py-2 bg-white rounded-t-lg ">
+                   <label for="comment" class="sr-only">Your comment</label>
+                   <textarea onChange={handlePromptChange} id="comment" rows="4" class="w-full px-0 text-sm text-gray-900 bg-white border-0 focus:ring-0 " placeholder="Write a comment..." required></textarea>
+               </div>
+               <div class="flex items-center justify-between px-3 py-2 border-t ">
+                   <button type="submit" class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 hover:bg-blue-800">
+                       Submit
+                   </button>
+               </div>
+           </div>
+        </form>
 
       {clarifyingQuestions.length > 0 && (
         <div>
-          <h2>Clarifying Questions:</h2>
-          {clarifyingQuestions.map((question, index) => (
-            <div key={index} className="mt-4">
-              <p>{question}</p>
-              <input
-                type="text"
-                value={userAnswers[index] || ""}
-                onChange={(event) => handleAnswerChange(event, index)}
-                className="border rounded px-4 py-2 w-full mt-2"
-              />
-            </div>
-          ))}
-          <button
-            type="submit"
-            onClick={handleClarifyingQuestionsSubmit}
-            className="bg-blue-900 text-white px-4 py-2 mt-4 rounded"
-          >
-            Submit Clarifying Answers
-          </button>
+         <h1 class="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl ">Clarifying Questions:</h1>
+          <p className="whitespace-pre-line p-5  ">
+          {clarifyingQuestions}
+          </p>
+            <form className="mx-4 ">
+               <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 ">
+                   <div class="px-4 py-2 bg-white rounded-t-lg ">
+                       <label for="comment" class="sr-only">Your comment</label>
+                       <textarea
+                        onChange={() => handleAnswerChange()}
+                        id="comment" rows="4" class="w-full px-0 text-sm text-gray-900 bg-white border-0 focus:ring-0 "
+                        placeholder="Add clarifying answers here" required>
+                       </textarea>
+                   </div>
+                   <div class="flex items-center justify-between px-3 py-2 border-t ">
+                       <button  onClick={handleClarifyingQuestionsSubmit} type="submit" class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 hover:bg-blue-800">
+                            Submit
+                       </button>
+                   </div>
+               </div>
+            </form>
         </div>
       )}
     </div>
