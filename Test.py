7883414a19681341aefcad2b349e9f0f4736f929
PrathamSutone <52961179+PrathamSutone@users.noreply.github.com>
2023-07-31 15:08:03 +0530

    Fixes

--- Test.py
+++ Test.py
@@ -1,7 +1,5 @@
 from utilities.folder_tree_structure import generate_folder_structure
-from utilities.create_project_summary import create_project_summary
-
-#print(generate_folder_structure("prathamthepro@gmail.com","10xdev.ai"))
+from utilities.files2analyse import files2analyse
 
 
-create_project_summary("hospital-project","prathamthepro@gmail.com")
+print(files2analyse('10xdev.ai',"prathamthepro@gmail.com"))
