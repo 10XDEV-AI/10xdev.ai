8dc3d9800b1a5e743ff04dfb48339634340101e4
PrathamSutone <52961179+PrathamSutone@users.noreply.github.com>
2023-08-06 01:26:06 +0530

    progress bar

--- utilities/mixpanel.py
+++ utilities/mixpanel.py
@@ -1,6 +1,6 @@
-import mixpanel
+from mixpanel import Mixpanel
 
-mixpanel_client = mixpanel.Mixpanel('597ffd5c8a8491e60f4373120abecddb')
+mixpanel_client = Mixpanel('597ffd5c8a8491e60f4373120abecddb')
 def track_event(event_name, properties):
     mixpanel_client.track(properties['email'], event_name, properties)
 
